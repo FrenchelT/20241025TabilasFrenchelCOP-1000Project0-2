c85f36578994d6abe784638b4a9c5e7c2cc905ae
FrenchelT <ftabilas@hawkmail.hccfl.edu>
2024-12-01 00:32:18 +0000

    20241025TabilasFrenchelCOP-1000Project0-2

--- main.py
+++ main.py
@@ -8,35 +8,38 @@ def print_allowed_vehicles():
     for vehicle in AllowedVehiclesList:
         print(f"- {vehicle}")
         print(" ")
+
 # Function to search for a specific vehicle
 def search_vehicle():
     print("********************************")
     print("Please Enter the full Vehicle name:")
-    vehicle_name = input("           ")
-    if vehicle_name in AllowedVehiclesList:
+    vehicle_name = input("           ").strip()
+    # Case-insensitive search logic
+    if vehicle_name.lower() in [vehicle.lower() for vehicle in AllowedVehiclesList]:
         print(f"{vehicle_name} is an authorized vehicle.")
     else:
         print(f"{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try again.")
     print("********************************")
+
 # Main menu function
 def menu():
     while True:
         print(" ")
         print("********************************")
-        print("AutoCountry Vehicle Finder v0.1")
+        print("AutoCountry Vehicle Finder v0.2")
         print("********************************")
         print(" ")
         print("Please Enter the following number below from the following menu:")
         print(" ")
-        print("1. Print all Authorized Vehicles")
-        print("2. Search for Authorized Vehicles")
+        print("1. PRINT all Authorized Vehicles")
+        print("2. SEARCH for Authorized Vehicle")
         print("3. Exit")
 
-        choice = input("           ")
+        choice = input("           ").strip()
 
         if choice == '1':
             print_allowed_vehicles()
-        elif choice== '2':
+        elif choice == '2':
             search_vehicle()
         elif choice == '3':
             print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
